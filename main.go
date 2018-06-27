82d0a4a23bb40159c3f7a81de059e70b315539c8
ad <daniel.apatin@gmail.com>
2018-06-27 14:29:18 +0300

    0.0.10

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ import (
 	"github.com/tatsushid/go-fastping"
 )
 
-const version = "0.0.9"
+const version = "0.0.10"
 
 func selfUpdate(slug string) error {
 	previous := semver.MustParse(version)
