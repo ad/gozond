ea3d93dd46c7a93275674ee0f02615e025bed794
ad <daniel.apatin@gmail.com>
2019-05-08 10:26:12 +0300

    version 0.1.1, added resolver to start params, some refactoring

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ import (
 	"github.com/tatsushid/go-fastping"
 )
 
-const version = "0.0.21"
+const version = "0.1.1"
 
 var pongStarted = abool.New()
 
@@ -40,13 +40,10 @@ func selfUpdate(slug string) error {
 		return err
 	}
 
-	if previous.Equals(latest.Version) {
-		// fmt.Println("Current binary is the latest version", version)
-	} else {
+	if !previous.Equals(latest.Version) {
 		fmt.Println("Update successfully done to version", latest.Version)
-		fmt.Println("Release note:\n", latest.ReleaseNotes)
 
-		restart()
+		Restart()
 	}
 
 	return nil
@@ -55,13 +52,15 @@ func selfUpdate(slug string) error {
 var zu, _ = uuid.NewV4()
 var addr = flag.String("addr", "localhost:80", "cc address:port")
 var zonduuid = flag.String("uuid", zu.String(), "zond uuid")
+var resolverAddress = flag.String("resolver", "8.8.8.8", "resolver address")
 
+// Action struct
 type Action struct {
-	ZondUuid string `json:"zond"`
+	ZondUUID string `json:"zond"`
 	Action   string `json:"action"`
 	Param    string `json:"param"`
 	Result   string `json:"result"`
-	Uuid     string `json:"uuid"`
+	UUID     string `json:"uuid"`
 }
 
 func main() {
@@ -74,7 +73,6 @@ func main() {
 			case <-ticker.C:
 				if err := selfUpdate("ad/gozond"); err != nil {
 					fmt.Fprintln(os.Stderr, err)
-					// os.Exit(1)
 				}
 			}
 		}
@@ -103,37 +101,40 @@ func main() {
 		for {
 			_, message, err := ws.ReadMessage()
 			if err != nil {
-				log.Println("read:", err)
+				log.Println("read error:", err)
 				time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
-				restart()
-				// return
+
+				Restart()
+
+				return
 			}
-			// log.Printf("recv: %s", message)
+
 			var action = new(Action)
 			err = json.Unmarshal(message, &action)
 			if err != nil {
-				fmt.Println("error:", err)
+				fmt.Println("unmarshal error:", err)
 			} else {
 				if action.Action != "alive" {
 					fmt.Printf("%+v\n", action)
 				}
 
 				if action.Action == "ping" {
-					pingCheck(action.Param, action.Uuid)
+					pingCheck(action.Param, action.UUID)
 				} else if action.Action == "head" {
-					headCheck(action.Param, action.Uuid)
+					headCheck(action.Param, action.UUID)
 				} else if action.Action == "dns" {
-					dnsCheck(action.Param, action.Uuid)
+					dnsCheck(action.Param, action.UUID)
 				} else if action.Action == "traceroute" {
-					tracerouteCheck(action.Param, action.Uuid)
+					tracerouteCheck(action.Param, action.UUID)
 				} else if action.Action == "alive" {
 					if !pongStarted.IsSet() {
 						pongStarted.Set()
-						ccAddr := *addr
-						action.ZondUuid = *zonduuid
+
+						action.ZondUUID = *zonduuid
 						js, _ := json.Marshal(action)
-						// log.Println("http://"+ccAddr+"/pong", string(js))
-						post("http://"+ccAddr+"/zond/pong", string(js))
+
+						Post("http://"+*addr+"/zond/pong", string(js))
+
 						pongStarted.UnSet()
 					}
 				}
@@ -149,7 +150,7 @@ func main() {
 			log.Println("interrupt")
 			err := ws.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
 			if err != nil {
-				log.Println("write close:", err)
+				log.Println("write close error:", err)
 				return
 			}
 			select {
@@ -161,23 +162,26 @@ func main() {
 	}
 }
 
-func restart() {
-	file, err := osext.Executable()
-	if err != nil {
-		log.Println("restart:", err)
-	} else {
-		err = syscall.Exec(file, os.Args, os.Environ())
-		if err != nil {
-			log.Fatal(err)
-		}
+// Restart app
+func Restart() error {
+	file, error := osext.Executable()
+	if error != nil {
+		return error
 	}
+
+	error = syscall.Exec(file, os.Args, os.Environ())
+	if error != nil {
+		return error
+	}
+
+	return nil
 }
 
 func pingCheck(address string, taskuuid string) {
 	ccAddr := *addr
-	var action = Action{ZondUuid: *zonduuid, Action: "block", Result: "", Uuid: taskuuid}
+	var action = Action{ZondUUID: *zonduuid, Action: "block", Result: "", UUID: taskuuid}
 	var js, _ = json.Marshal(action)
-	var status = post("http://"+ccAddr+"/zond/task/block", string(js))
+	var status = Post("http://"+*addr+"/zond/task/block", string(js))
 
 	if status != `{"status": "ok", "message": "ok"}` {
 		if status == `{"status": "error", "message": "only one task at time is allowed"}` {
@@ -191,10 +195,10 @@ func pingCheck(address string, taskuuid string) {
 		ra, err := net.ResolveIPAddr("ip4:icmp", address)
 		if err != nil {
 			fmt.Println(address+" ping failed: ", err)
-			action := Action{ZondUuid: *zonduuid, Action: "result", Result: fmt.Sprintf("failed: %s", err), Uuid: taskuuid}
+			action := Action{ZondUUID: *zonduuid, Action: "result", Result: fmt.Sprintf("failed: %s", err), UUID: taskuuid}
 			js, _ := json.Marshal(action)
 
-			post("http://"+ccAddr+"/zond/task/result", string(js))
+			Post("http://"+ccAddr+"/zond/task/result", string(js))
 		} else {
 			p.AddIPAddr(ra)
 			var received = false
@@ -202,18 +206,18 @@ func pingCheck(address string, taskuuid string) {
 				received = true
 				fmt.Printf("IP Addr: %s receive, RTT: %v\n", addr.String(), rtt)
 
-				action = Action{ZondUuid: *zonduuid, Action: "result", Result: rtt.String(), Uuid: taskuuid}
+				action = Action{ZondUUID: *zonduuid, Action: "result", Result: rtt.String(), UUID: taskuuid}
 				js, _ = json.Marshal(action)
 
-				post("http://"+ccAddr+"/zond/task/result", string(js))
+				Post("http://"+ccAddr+"/zond/task/result", string(js))
 			}
 			p.OnIdle = func() {
 				if !received {
 					fmt.Println(address + " ping failed")
-					action := Action{ZondUuid: *zonduuid, Action: "result", Result: "failed", Uuid: taskuuid}
+					action := Action{ZondUUID: *zonduuid, Action: "result", Result: "failed", UUID: taskuuid}
 					js, _ := json.Marshal(action)
 
-					post("http://"+ccAddr+"/zond/task/result", string(js))
+					Post("http://"+ccAddr+"/zond/task/result", string(js))
 				}
 			}
 			err = p.Run()
@@ -226,12 +230,11 @@ func pingCheck(address string, taskuuid string) {
 
 func dnsCheck(address string, taskuuid string) {
 	ccAddr := *addr
-	var action = Action{ZondUuid: *zonduuid, Action: "block", Result: "", Uuid: taskuuid}
+	var action = Action{ZondUUID: *zonduuid, Action: "block", Result: "", UUID: taskuuid}
 	var js, _ = json.Marshal(action)
-	var status = post("http://"+ccAddr+"/zond/task/block", string(js))
+	var status = Post("http://"+ccAddr+"/zond/task/block", string(js))
 
 	if status != `{"status": "ok", "message": "ok"}` {
-
 		if status == `{"status": "error", "message": "only one task at time is allowed"}` {
 			time.Sleep(time.Duration(rand.Intn(10)) * time.Second)
 			dnsCheck(address, taskuuid)
@@ -239,12 +242,12 @@ func dnsCheck(address string, taskuuid string) {
 			log.Println(taskuuid, status)
 		}
 	} else {
-		var resolver_address = "8.8.8.8"
+		// var resolverAddress = "8.8.8.8"
 		if strings.Count(address, "-") == 1 {
 			s := strings.Split(address, "-")
-			address, resolver_address = s[0], s[1]
+			address, *resolverAddress = s[0], s[1]
 		}
-		resolver := dns_resolver.New([]string{resolver_address})
+		resolver := dns_resolver.New([]string{*resolverAddress})
 		// resolver := dns_resolver.NewFromResolvConf("resolv.conf")
 		resolver.RetryTimes = 5
 
@@ -252,10 +255,10 @@ func dnsCheck(address string, taskuuid string) {
 
 		if err != nil {
 			log.Println(address+" dns failed: ", err)
-			action := Action{ZondUuid: *zonduuid, Action: "result", Result: fmt.Sprintf("failed: %s", err), Uuid: taskuuid}
+			action := Action{ZondUUID: *zonduuid, Action: "result", Result: fmt.Sprintf("failed: %s", err), UUID: taskuuid}
 			js, _ := json.Marshal(action)
 
-			post("http://"+ccAddr+"/zond/task/result", string(js))
+			Post("http://"+ccAddr+"/zond/task/result", string(js))
 		} else {
 			var s []string
 			for _, ip := range ips {
@@ -264,19 +267,19 @@ func dnsCheck(address string, taskuuid string) {
 			var res = strings.Join(s[:], ",")
 			log.Printf("IPS: %v", res)
 
-			action = Action{ZondUuid: *zonduuid, Action: "result", Result: res, Uuid: taskuuid}
+			action = Action{ZondUUID: *zonduuid, Action: "result", Result: res, UUID: taskuuid}
 			js, _ = json.Marshal(action)
 
-			post("http://"+ccAddr+"/zond/task/result", string(js))
+			Post("http://"+ccAddr+"/zond/task/result", string(js))
 		}
 	}
 }
 
 func tracerouteCheck(address string, taskuuid string) {
 	ccAddr := *addr
-	var action = Action{ZondUuid: *zonduuid, Action: "block", Result: "", Uuid: taskuuid}
+	var action = Action{ZondUUID: *zonduuid, Action: "block", Result: "", UUID: taskuuid}
 	var js, _ = json.Marshal(action)
-	var status = post("http://"+ccAddr+"/zond/task/block", string(js))
+	var status = Post("http://"+ccAddr+"/zond/task/block", string(js))
 
 	if status != `{"status": "ok", "message": "ok"}` {
 		if status == `{"status": "error", "message": "only one task at time is allowed"}` {
@@ -294,10 +297,10 @@ func tracerouteCheck(address string, taskuuid string) {
 
 		if err != nil {
 			log.Println(address+" traceroute failed: ", err)
-			action := Action{ZondUuid: *zonduuid, Action: "result", Result: fmt.Sprintf("failed: %s", err), Uuid: taskuuid}
+			action := Action{ZondUUID: *zonduuid, Action: "result", Result: fmt.Sprintf("failed: %s", err), UUID: taskuuid}
 			js, _ := json.Marshal(action)
 
-			post("http://"+ccAddr+"/zond/task/result", string(js))
+			Post("http://"+ccAddr+"/zond/task/result", string(js))
 		} else {
 			var s []string
 
@@ -308,19 +311,19 @@ func tracerouteCheck(address string, taskuuid string) {
 			var res = strings.Join(s[:], "\n")
 			log.Printf("Result: %v", res)
 
-			action = Action{ZondUuid: *zonduuid, Action: "result", Result: res, Uuid: taskuuid}
+			action = Action{ZondUUID: *zonduuid, Action: "result", Result: res, UUID: taskuuid}
 			js, _ = json.Marshal(action)
 
-			post("http://"+ccAddr+"/zond/task/result", string(js))
+			Post("http://"+ccAddr+"/zond/task/result", string(js))
 		}
 	}
 }
 
 func headCheck(address string, taskuuid string) {
 	ccAddr := *addr
-	var action = Action{ZondUuid: *zonduuid, Action: "block", Result: "", Uuid: taskuuid}
+	var action = Action{ZondUUID: *zonduuid, Action: "block", Result: "", UUID: taskuuid}
 	var js, _ = json.Marshal(action)
-	var status = post("http://"+ccAddr+"/zond/task/block", string(js))
+	var status = Post("http://"+ccAddr+"/zond/task/block", string(js))
 
 	if status != `{"status": "ok", "message": "ok"}` {
 		if status == `{"status": "error", "message": "only one task at time is allowed"}` {
@@ -335,49 +338,45 @@ func headCheck(address string, taskuuid string) {
 			defer resp.Body.Close()
 		}
 		if err != nil {
-			// fmt.Println(address+" http head failed: ", err)
 			log.Println(address+" http head failed: ", err)
-			action := Action{ZondUuid: *zonduuid, Action: "result", Result: fmt.Sprintf("failed: %s", err), Uuid: taskuuid}
+			action := Action{ZondUUID: *zonduuid, Action: "result", Result: fmt.Sprintf("failed: %s", err), UUID: taskuuid}
 			js, _ := json.Marshal(action)
 
-			post("http://"+ccAddr+"/zond/task/result", string(js))
+			Post("http://"+ccAddr+"/zond/task/result", string(js))
 		} else {
 			headers := resp.Header
 			var s string
 			for key, val := range headers {
 				s += fmt.Sprintf("%s: %s\n", key, val)
 			}
-			// fmt.Printf("Headers: %v\n", s)
 			log.Printf("Headers: %v", s)
 
-			action = Action{ZondUuid: *zonduuid, Action: "result", Result: s, Uuid: taskuuid}
+			action = Action{ZondUUID: *zonduuid, Action: "result", Result: s, UUID: taskuuid}
 			js, _ = json.Marshal(action)
 
-			post("http://"+ccAddr+"/zond/task/result", string(js))
+			Post("http://"+ccAddr+"/zond/task/result", string(js))
 		}
 	}
 }
 
-func get(url string) string {
+// Get request and return response
+func Get(url string) string {
 	resp, err := http.Get(url)
 	if resp != nil {
 		defer resp.Body.Close()
 	}
-	if err != nil {
-		fmt.Printf("%s", err)
-		// os.Exit(1)
-	} else {
+	if err == nil {
 		contents, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
-			fmt.Printf("%s", err)
 			os.Exit(1)
 		}
 		return string(contents)
 	}
-	return ""
+	return "error"
 }
 
-func post(url string, jsonData string) string {
+// Post request with headers and return response
+func Post(url string, jsonData string) string {
 	var jsonStr = []byte(jsonData)
 
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
@@ -390,18 +389,14 @@ func post(url string, jsonData string) string {
 		defer resp.Body.Close()
 	}
 	if err != nil {
-		log.Println(err)
 		return "error"
-	} else {
-		if resp.StatusCode == 429 {
-			log.Printf("%s: %d", url, resp.StatusCode)
-			time.Sleep(time.Duration(rand.Intn(30)) * time.Second)
-			return post(url, jsonData)
-		} else {
-			// fmt.Println("response Status:", resp.Status)
-			// fmt.Println("response Headers:", resp.Header)
-			body, _ := ioutil.ReadAll(resp.Body)
-			return string(body)
-		}
 	}
+
+	if resp.StatusCode == 429 {
+		log.Printf("%s: %d", url, resp.StatusCode)
+		time.Sleep(time.Duration(rand.Intn(30)) * time.Second)
+		return Post(url, jsonData)
+	}
+	body, _ := ioutil.ReadAll(resp.Body)
+	return string(body)
 }
